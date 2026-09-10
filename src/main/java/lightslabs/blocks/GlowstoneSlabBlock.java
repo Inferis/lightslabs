4327e403d4c1a1f3303f81ed6ed75846d265a751
Tom <tom@inferis.org>
2026-09-10 12:37:52 -0700

    Don't need GlowstoneSlabBlock.

--- src/main/java/lightslabs/blocks/GlowstoneSlabBlock.java
+++
@@ -1,9 +0,0 @@
-package lightslabs.blocks;
-
-import net.minecraft.world.level.block.SlabBlock;
-
-public class GlowstoneSlabBlock extends SlabBlock {
-    public GlowstoneSlabBlock(Properties properties) {
-        super(properties);
-    }
-}
